cc393afd7fdc6347708522753d51e38c801eb638
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-06-08 14:12:18 +0700

    Update cosine-similarity.js

--- cosine-similarity.js
+++ cosine-similarity.js
@@ -1,6 +1,6 @@
 function cosineSimilarity(str1, str2, term = false) {
-	let str1Words = term ? str1.trim().split(" ").map(w => {return removeAccents(w.toLowerCase()).replace(/[^a-zA-Z0-9 ]/g, '')}) : removeAccents(str1.trim().toLowerCase().replace(/\s+/g, '')).replace(/[^a-zA-Z0-9 ]/g, '');
-	let str2Words = term ? str2.trim().split(" ").map(w => {return removeAccents(w.toLowerCase()).replace(/[^a-zA-Z0-9 ]/g, '')}) : removeAccents(str2.trim().toLowerCase().replace(/\s+/g, '')).replace(/[^a-zA-Z0-9 ]/g, '');
+	let str1Words = term ? str1.trim().split(" ").map(w => {return w.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '')}) : str1.trim().toLowerCase().replace(/\s+/g, '').replace(/[^a-zA-Z0-9 ]/g, '');
+	let str2Words = term ? str2.trim().split(" ").map(w => {return w.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '')}) : str2.trim().toLowerCase().replace(/\s+/g, '').replace(/[^a-zA-Z0-9 ]/g, '');
 	let dict = Array.from(new Set(str1Words.concat(str2Words)));
 	let str1Vector = strToVector(str1Words, dict);
 	let str2Vector = strToVector(str2Words, dict);
@@ -22,18 +22,18 @@ function strToVector(str, dict) {
 
 function cosineCal(A, B) {
    	let dotProduct = 0;
-   	let mA = 0, mB = 0;
-    	for (let i = 0; i < A.length; i++) {
-        	dotProduct += (A[i] * B[i]);
-        	mA += (A[i]*A[i]);
-        	mB += (B[i]*B[i]);
-   	}
-    	let cosine = (dotProduct)/((Math.sqrt(mA))*(Math.sqrt(mB)));
-   	return cosine;
+    let mA = 0, mB = 0;
+    for (let i = 0; i < A.length; i++) {
+        dotProduct += (A[i] * B[i]);
+        mA += (A[i]*A[i]);
+        mB += (B[i]*B[i]);
+    }
+    let cosine = (dotProduct)/((Math.sqrt(mA))*(Math.sqrt(mB)));
+    return cosine;
 }
 
-function removeAccents(str){
+/*function removeAccents(str){
 	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/đ/g, 'd').replace(/Đ/g, 'D');
-}
+}*/
 
 module.exports = cosineSimilarity;
